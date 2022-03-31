9b8c0dbc398d6d90e08d02de45041eff9e242f80
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-04-01 00:31:03 +0200

    Added db context to connect to sqlite database

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using CarRentalRestApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalRestApi.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarRentalRestApi.Data;
 using CarRentalRestApi.Services.VehicleService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,8 @@ namespace CarRentalRestApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnectionSqlite")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
