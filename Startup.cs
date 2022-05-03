a293fa81071d74cfb49493b199b02d1c9e10487a
Jan Piaskowy <piaskowyjasiek@gmail.com>
2022-05-03 22:19:28 +0200

    Added migration to git and cores setings

--- Startup.cs
+++ Startup.cs
@@ -32,6 +32,10 @@ namespace CarRentalRestApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors();
+            services.AddControllers();
+            
             services.AddDbContext<DataContext>(options =>
                 options.UseSqlite(_configuration.GetConnectionString("DefaultConnectionSqlite")));
             services.AddControllers();
@@ -94,6 +98,12 @@ namespace CarRentalRestApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials()); 
             
             app.UseAuthentication();
 
